2b76d01deb54b607dc762f733016ae59ad8504e0
Rushika David Jayasinghe <96676141+Rushika08@users.noreply.github.com>
2023-05-27 16:48:35 +0530

    Update and rename .gitignore to PartA.cpp

---
+++ PartA.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int pattern(int n, int initial)
+{
+	int element = initial + 3;
+	n--;
+	
+	if(n>1)
+		pattern(n,element);
+		
+	else if(n==0)
+		return 1;
+		
+	else	
+		return element;
+}
+
+int main()
+{
+	int number;
+	cout<<"Enter the number(n) - ";
+	cin>>number;
+	
+	cout<<"The "<<number<<" element of the pattern is "<<pattern(number,1);
+	
+	return 0;
+}
